fac09dcaf559b92d1c7a8fa9edb964e44b1a4b66
Beyda <beydaacar1@gmail.com>
2020-04-29 00:04:10 +0300

    certain parts are completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function takip() {
+    alert("Takip edildi.")
+}
+function yorum() {
+    alert("Yorum yapıldı.")
+}
+function retweet() {
+    alert("Retweet yapıldı.")
+}
+function begen() {
+    alert("Beğenildi.")
+}
+function paylas() {
+    alert("Link kopyalandı.")
+}
+function tweetlendi() {
+    alert("Tweet gönderildi.")
+}
+function olustur() {
+    alert("Liste oluşturuldu.")
+}
